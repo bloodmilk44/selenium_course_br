3d8464bc17e6926ec4758797a730f8522b682682
bloodmilk44 <59761531+bloodmilk44@users.noreply.github.com>
2020-03-29 19:23:33 +0400

    улучшены тесты

--- test_add_group.py
+++ test_add_group.py
@@ -1,59 +1,24 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
 from group import Group
+from application import Application
+import pytest
 
-class TestCreatenewgroup():
-  def setup_method(self, method):
-    self.driver = webdriver.Firefox()
-    self.vars = {}
+
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
   
-  def teardown_method(self, method):
-    self.driver.quit()
-  
-  def test_createnewgroup(self):
-    self.open_home_page()
-    self.login(username="admin", password="secret")
-    self.open_group_page()
-    self.create_group(Group(name="ggg43g34g43g34", header="g34g43g43g43", footer="egwgegewgwegwe"))
-    self.return_to_group_page()
-    self.logout()
+def test_createnewgroup(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="ggg43g34g43g34", header="g34g43g43g43", footer="egwgegewgwegwe"))
+    app.logout()
 
-  def test_add_empty_group(self):
-    self.open_home_page()
-    self.login(username="admin", password="secret")
-    self.open_group_page()
-    self.create_group(Group(name="", header="", footer=""))
-    self.return_to_group_page()
-    self.logout()
+def test_add_empty_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="", header="", footer=""))
+    app.logout()
 
-  def logout(self):
-    self.driver.find_element(By.LINK_TEXT, "Logout").click()
 
-  def return_to_group_page(self):
-    self.driver.find_element(By.LINK_TEXT, "group page").click()
-
-  def create_group(self, group):
-    # init group creation
-    self.driver.find_element(By.NAME, "new").click()
-    # fill group form
-    self.driver.find_element(By.NAME, "group_name").click()
-    self.driver.find_element(By.NAME, "group_name").send_keys(group.name)
-    self.driver.find_element(By.NAME, "group_header").click()
-    self.driver.find_element(By.NAME, "group_header").send_keys(group.header)
-    self.driver.find_element(By.NAME, "group_footer").click()
-    self.driver.find_element(By.NAME, "group_footer").send_keys(group.footer)
-    # submit group creation
-    self.driver.find_element(By.NAME, "submit").click()
-
-  def open_group_page(self):
-    self.driver.find_element(By.LINK_TEXT, "groups").click()
-
-  def login(self, username, password):
-    self.driver.set_window_size(1108, 822)
-    self.driver.find_element(By.NAME, "user").send_keys(username)
-    self.driver.find_element(By.NAME, "pass").send_keys(password)
-    self.driver.find_element(By.CSS_SELECTOR, "input:nth-child(7)").click()
-
-  def open_home_page(self):
-    self.driver.get("http://localhost/addressbook/")
   
