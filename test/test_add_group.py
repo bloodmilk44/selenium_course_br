57a74cd1d9e2943b1fd23043cdf325117f6254ab
bloodmilk44 <59761531+bloodmilk44@users.noreply.github.com>
2020-04-13 21:11:58 +0400

    добавил интеллектуальную фикстуру

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -2,11 +2,7 @@ from model.group import Group
 
 
 def test_createnewgroup(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(name="ggg43g34g43g34", header="g34g43g43g43", footer="egwgegewgwegwe"))
-    app.session.logout()
 
 def test_add_empty_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(name="", header="", footer=""))
-    app.session.logout()
