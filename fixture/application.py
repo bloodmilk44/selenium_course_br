6c48387cfd41458ef64539224013b5e8893fdcd2
bloodmilk44 <59761531+bloodmilk44@users.noreply.github.com>
2020-04-30 16:48:54 +0400

    Добавил опции для выбора браузера и базовой страницы

--- fixture/application.py
+++ fixture/application.py
@@ -6,13 +6,21 @@ from fixture.contact import ContactHelper
 
 class Application:
 
-    def __init__(self):
-        self.driver = webdriver.Firefox()
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.driver = webdriver.Firefox()
+        elif browser == "chrome":
+            self.driver = webdriver.Chrome()
+        elif browser == "ie":
+            self.driver = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
         self.driver.implicitly_wait(3)
         self.vars = {}
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -22,7 +30,7 @@ class Application:
             return False
 
     def open_home_page(self):
-        self.driver.get("http://localhost/addressbook/")
+        self.driver.get(self.base_url)
 
     def destroy(self):
         self.driver.quit()
