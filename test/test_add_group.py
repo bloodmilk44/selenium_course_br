56a9c4fbfa47f551c142ec5f6e879fe21bdc2336
bloodmilk44 <59761531+bloodmilk44@users.noreply.github.com>
2020-05-01 19:07:08 +0400

    реализовано чтение тестовых данных из json

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,8 +1,8 @@
 from model.group import Group
 
 
-def test_createnewgroup(app, data_groups):
-    group = data_groups
+def test_createnewgroup(app, json_groups):
+    group = json_groups
     old_groups = app.group.get_group_list()
     app.group.create(group)
     assert len(old_groups) + 1 == app.group.count()
